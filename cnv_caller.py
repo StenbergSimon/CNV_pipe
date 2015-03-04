88c027f0c9f59a4722721986fc717bca628c5199
StenbergSimon <simon.stenberg@nmbu.no>
2015-03-04 11:43:58 +0100

    Removed printing of chr list path

--- cnv_caller.py
+++ cnv_caller.py
@@ -286,7 +286,6 @@ if __name__ == "__main__":
     options.path = setAbsPath(options)
     normalizer = 0 
 
-    print options.order
     if not os.path.exists(options.path):
        os.makedirs(options.path)
  
